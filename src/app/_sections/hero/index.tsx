7fcde7fe345984510ba1c426724be2989bf6e694
Buddy Agyin <20474715+agyin3@users.noreply.github.com>
2024-12-20 17:18:29 -0700

    update the bg

--- src/app/_sections/hero/index.tsx
+++ src/app/_sections/hero/index.tsx
@@ -1,6 +1,11 @@
+import styles from "./hero.module.css";
+
 export const Hero = () => {
     return (
-        <div id="home" className="h-full flex items-center justify-center">
+        <div
+            id="home"
+            className={`h-full flex items-center justify-center ${styles.heroBg}`}
+        >
             <h1 className="text-7xl font-bold text-center sm:text-9xl">
                 <span className="bg-clip-text text-transparent bg-gradient-to-tr from-secondary to-accent1 block leading-normal">
                     Create
