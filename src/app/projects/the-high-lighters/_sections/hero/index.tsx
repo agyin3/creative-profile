db8726b12db753cfd8ef4619a459d4992530d402
Buddy Agyin <20474715+agyin3@users.noreply.github.com>
2025-01-13 15:09:55 -0700

    remove high lighters stuff

--- src/app/projects/the-high-lighters/_sections/hero/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./hero.module.css";
-
-export function TheHighLightersHero() {
-    return (
-        <div
-            className={`w-full h-96 bg-black text-white flex justify-center items-center ${styles.highlightersHero}`}
-        ></div>
-    );
-}
